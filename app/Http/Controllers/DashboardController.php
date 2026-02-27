c76f3737fe81024b3c4fb78b591ddd42b4117d61
Reyli Adrian <reyliperez29@gmail.com>
2026-02-27 09:30:09 -0600

    feat: estructura inicial del proyecto seleccionado

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        return "Bienvenido al Sistema de Administración de Internet";
+    }
+}
